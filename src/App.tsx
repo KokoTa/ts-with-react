ddc9bdc2860c32b10b1efbd5779c8c8abac52692
KokoTa <584847514@qq.com>
2021-04-23 17:54:40 +0800

    💄 style: 基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 /*
  * @Author: KokoTa
  * @Date: 2021-04-21 16:29:12
- * @LastEditTime: 2021-04-23 11:08:16
+ * @LastEditTime: 2021-04-23 17:54:14
  * @LastEditors: KokoTa
  * @Description: 
  * @FilePath: /ts-with-react/src/App.tsx
  */
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
 import UseStateDemo from './components/hooks/UseStateDemo'
 import UseEffectDemo from './components/hooks/UseEffectDemo'
 import UseRefDemo from './components/hooks/UseRefDemo'
@@ -20,23 +17,20 @@ function App() {
   const [showUseEffectDemo, setShowUseEffectDemo] = useState(true)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p className="title">useState demo</p>
+    <div>
+      <header>
+        <h1>useState demo</h1>
         <UseStateDemo></UseStateDemo>
 
-        <p className="title">useEffect demo</p>
+        <h1>useEffect demo</h1>
         <button onClick={() => setShowUseEffectDemo(!showUseEffectDemo)}>视图切换</button>
         {showUseEffectDemo && <UseEffectDemo></UseEffectDemo>}
 
-        <p className="title">useRef demo</p>
+        <h1>useRef demo</h1>
         <UseRefDemo></UseRefDemo>
 
-        <p className="title">useContext demo</p>
+        <h1>useContext demo</h1>
         <UseContextDemo></UseContextDemo>
-
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+/*
+ * @Author: KokoTa
+ * @Date: 2021-04-21 16:29:12
+ * @LastEditTime: 2021-04-23 17:44:48
+ * @LastEditors: KokoTa
+ * @Description: 
+ * @FilePath: /ts-with-react/src/index.tsx
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
